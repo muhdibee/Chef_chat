b90ebcb0af66016b243de30c0c15b5c5677eaf4b
Muhammad Ibrahim <muhdibee@gmail.com>
2023-03-18 17:51:56 +0100

    Connecting client.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-// const path = require('path')
-import express from 'express';
-import http from 'http';
-import {log} from './log.js';
-import {Server} from 'socket.io'
+// import {log} from './log.js';
+const path = require("path")
+const express = require("express");
+const app = express();
+const http = require("http");
 
-const app  = express()
 const server = http.createServer(app);
-const io = new Server(300);
+const port = 8000;
 
-console.log(typeof log);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-
-const port = 3000;
 app.get('/', (req,res) => {
+    // res.send("Hello");
     res.sendFile(path.join(__dirname, "index.html"))
 
 });
 
-io.on('connection', (socket)=>{
-    log("socket object", socket.id)
+io.on("connection", (socket)=>{
+    console.log("socket object connected");
     socket.emit("connected", "data");
 })
 
 server.listen(port, ()=> {
-    log("Running on port", port);
+console.log(`Running on port ${port}`);
 });
