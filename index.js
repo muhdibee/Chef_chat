ce242a3e19be5292222feba0366e05c2c3245bea
Muhammad Ibrahim <muhdibee@gmail.com>
2023-03-18 05:00:43 +0100

    Simple server.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const http = require('http');
-const app = require ('express')()
-const Server = require('socket.io');
+// const path = require('path')
+import express from 'express';
+import http from 'http';
+import {log} from './log.js';
+
+const app  = express()
 const server = http.createServer(app);
-const io = Server(server);
+// const io = require('socket.io')(server);
+
+console.log(typeof log);
 
 
+const port = 3000;
+app.get('/', (req,res) => {
+    // res.sendFile(path.join(__dirname, "index.html"))
+    res.send("Hello");
+
+});
+
+server.listen(port, ()=> {
+    log("Running on port", port);
+});
