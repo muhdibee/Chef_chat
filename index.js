0e9d51973732e975005a4fd98c575b35ab8109fb
Muhammad Ibrahim <muhdibee@gmail.com>
2023-03-18 09:20:45 +0100

    working connection.

--- index.js
+++ index.js
@@ -2,20 +2,25 @@
 import express from 'express';
 import http from 'http';
 import {log} from './log.js';
+import {Server} from 'socket.io'
 
 const app  = express()
 const server = http.createServer(app);
-// const io = require('socket.io')(server);
+const io = new Server(300);
 
 console.log(typeof log);
 
 
 const port = 3000;
 app.get('/', (req,res) => {
-    // res.sendFile(path.join(__dirname, "index.html"))
-    res.send("Hello");
+    res.sendFile(path.join(__dirname, "index.html"))
 
 });
+
+io.on('connection', (socket)=>{
+    log("socket object", socket.id)
+    socket.emit("connected", "data");
+})
 
 server.listen(port, ()=> {
     log("Running on port", port);
