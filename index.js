a107e5087630e9f04b6493832900919662283cb9
Muhammad Ibrahim <muhdibee@gmail.com>
2023-03-19 12:26:45 +0100

    Working on front-end.

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 // import {log} from './log.js';
-const path = require("path")
-const express = require("express");
-const app = express();
-const http = require("http");
+// const path = require("path")
+// const express = require("express");
+// const app = express();
+// const http = require("http");
 
-const server = http.createServer(app);
-const port = 8000;
+// const server = http.createServer(app);
+// const port = 8000;
 
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
-app.get('/', (req,res) => {
-    // res.send("Hello");
-    res.sendFile(path.join(__dirname, "index.html"))
+// app.get('/', (req,res) => {
+//     // res.send("Hello");
+//     res.sendFile(path.join(__dirname, "index.html"))
 
-});
+// });
 
-io.on("connection", (socket)=>{
-    console.log("socket object connected");
-    socket.emit("connected", "data");
-})
+// io.on("connection", async (socket)=>{
+//     console.log(`client ${socket.id} connected`);
+//     socket.emit("connected", {id:socket.id });
 
-server.listen(port, ()=> {
-console.log(`Running on port ${port}`);
-});
+
+//     socket.on("disconnect", async ()=>{
+//         console.log("client disconnected.")
+//     })
+// });
+
+// server.listen(port, ()=> {
+// console.log(`Running on port ${port}`);
+// });
+
