9073ac70beed5c603ef92958610096d3d8887688
Muhammad Ibrahim <muhdibee@gmail.com>
2023-03-21 04:35:47 +0100

    Styled chatSpace and bot response.

--- public/Index-script.js
+++ public/Index-script.js
@@ -2,25 +2,28 @@ const socket = io();
 
 // Collecting DOM elements.
 let body = document.getElementsByTagName('body');
-let chatSpace = document.getElementById("chatSpace");
+// let chatSpace = document.getElementById("chatSpace");
 let chatbotForm = document.getElementById("chatbotForm");
 let inputField = document.getElementById("inputField")
 
 // Creating DOM element.
-let botParag = document.createElement("p");
+// let botParag = document.createElement("p");
 let userParag = document.createElement("p");
 
 // Adding CSS
-let botParagCSS = `
-    position: absolute; 
+let botDivgCSS = `
+    position: relative; 
     left:2%;
-    width: 60%;
-    padding: 2%;
-    background-color: rgb(74, 106, 129);
+    min-width: 40;
+    max-width: 60%;
+    padding: 1%;
+    margin: 15px;
+    color: antiquewhite;
+    background-color: rgb(56, 97, 126);
     border-radius:10px;
     border-bottom-left-radius: 0px;
 `
-let userParagCSS = `
+let userDivCSS = `
     position: absolute; 
     left:2%;
     width: 60%;
@@ -29,12 +32,6 @@ let userParagCSS = `
     border-radius:20px;
     border-bottom-left-radius: 0px;
 `
-//  creating classes for reusability.
-// class BotParag {
-//     constructor(){
-
-//     }
-// }
 
 // Global variables.
 let invalidResponse = `
@@ -44,32 +41,52 @@ let invalidResponse = `
     Select <b>98</b> to see order history</br>
     Select <b>97</b> to see current order</br>
     Select <b>0</b> to cancel order
-`
+    `
+    // Append bot message to chat.
+    const appendbotMessage = (message) => {
+        let chatSpace = document.getElementById("chatSpace");
+        let botDiv = document.createElement("div");
+        botDiv.innerHTML = `<p>${message}</P>`
+        botDiv.style.cssText = botDivgCSS
+        chatSpace.append(botDiv);
+  }
+
+    // Append user message to chat.
+    const appenduserMessage = (message) => {
+        let chatSpace = document.getElementById("chatSpace");
+        let botDiv = document.createElement("div");
+        botDiv.innerHTML = `<p>${message}</P>`
+        botDiv.style.cssText = botDivgCSS
+        chatSpace.append(botDiv);
+  }
+
+
 
 userParag.style.cssText = userParagCSS;
 
 
-// On initial connection to server
+// On initial connection to server.
 socket.on("connected", async (initailBotMessage)=>{
     console.log("Connected to server.");
     console.log(initailBotMessage);
     
-    botParag.innerHTML = `${initailBotMessage}`
-    chatSpace.append(botParag);
-    botParag.style.cssText = botParagCSS
+    appendbotMessage(initailBotMessage);
+    
 })
 
 
 // Handling form submission.
 chatbotForm.addEventListener("submit", (e)=>{
     e.preventDefault();
-    console.log(inputField.value)
-    if (inputField.value != 0 || 1 || 97 || 98 || 99){
-        inputField.value = "";
-
-        return chatSpace.append(invalidResponse);
+    const userMessage = inputField.value;
+    console.log(userMessage)
+    if (userMessage != 0 || 1 || 97 || 98 || 99){
+        userMessage = "";
+        appenduserMessage(userMessage);
+        appendbotMessage(invalidResponse);
+        return
     }
-    socket.emit("userMessage", userMessage.value);
+    socket.emit("userMessage", userMessage);
 })
 
 
