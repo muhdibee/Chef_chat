a75d697b352e753c50ccbba0df0ab9eed58fe78a
Muhammad Ibrahim <muhdibee@gmail.com>
2023-03-27 14:55:39 +0100

    Moved index-script.js code to index.html.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const http = require("http");
 
 const server = http.createServer(app);
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const { Server } = require("socket.io");
 const io = new Server(server);
